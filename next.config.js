c44716951cb15f5d0cb4bbaee6e553e4eb41b745
Syn Zhang <zeyanzhang.china@gmail.com>
2018-12-18 00:48:06 +0800

    Support sass

--- next.config.js
+++ next.config.js
@@ -2,12 +2,13 @@ require('dotenv').config();
 
 const webpack = require('webpack');
 const withCss = require('@zeit/next-css');
+const withSass = require('@zeit/next-sass');
 
 if (typeof require !== 'undefined') {
   require.extensions['.css'] = (file) => {}
 };
 
-module.exports = withCss({
+module.exports = withCss(withSass({
   webpack: (config) => {
     config.plugins.push(
       new webpack.EnvironmentPlugin(process.env)
@@ -20,4 +21,4 @@ module.exports = withCss({
   publicRuntimeConfig: {
     apiUrlBase: process.env.API_URL_BASE
   },
-});
+}));
